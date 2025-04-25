51bc855db14b01c403b74f4b18aac4aea2f6365d
Asad Ghaffar <fa21-bcs-015@isbstudent.comsats.edu.pk>
2025-04-25 15:32:14 +0500

    Vite Config adjusted.

--- routes/web.php
+++ routes/web.php
@@ -37,8 +37,8 @@ Route::post('register', [RegisterController::class, 'register']);
 // Password Reset Routes
 Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
 Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
-Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
-Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
+// Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+// Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
 
 // Dynamic Dropdown for Stations
 Route::get('/stations/by-region', [StationController::class, 'getStationsByRegion'])->name('stations.by-region');
